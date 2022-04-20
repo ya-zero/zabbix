a7b920bc67445fce09f77b4737ac01f9d6e943cb
ya-zero <yazero@yandex.ru>
2022-04-21 00:06:26 +0300

    Delete zabbix1.py

--- zabbix1.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from pyzabbix import ZabbixAPI
-
-connect=ZabbixAPI('http://172.20.103.201', user='zero',password='1alexander2')
-zabbix= connect.api_version()
-print (zabbix)
-
